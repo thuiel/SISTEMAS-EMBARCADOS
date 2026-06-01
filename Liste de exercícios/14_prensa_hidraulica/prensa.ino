ddb466e3ce5f95b551762ace24fe5ed7063e830e
Arthur Schultz Thiel <130780416+thuiel@users.noreply.github.com>
2026-06-01 13:21:15 -0300

    Update prensa.ino

--- Liste de exercícios/14_prensa_hidraulica/prensa.ino
+++ Liste de exercícios/14_prensa_hidraulica/prensa.ino
@@ -1,18 +1,3 @@
-/* ************************************************************************
-
-Colégio SATC
-Curso Técnico em Mecatrônica
-Disciplina: Sistemas Embarcados
-Turma: 3191
-Professor: Marcos Antonio Jeremias Coelho
-
-Programa: Prensa Hidráulica com Comando Bi-manual
-
-Autor: Tiago Fortunato Rosset
-Data: 12/04/2026
-Versão: 1.0
-
-************************************************************************ */
 
 const int botao1 = 2;
 const int botao2 = 3;
