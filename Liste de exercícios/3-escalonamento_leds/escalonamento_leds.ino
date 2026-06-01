ffc103fdb582fc19d3d7976582ac250d44f96c2b
Arthur Schultz Thiel <130780416+thuiel@users.noreply.github.com>
2026-06-01 13:42:38 -0300

    Update escalonamento_leds.ino

--- Liste de exercícios/3-escalonamento_leds/escalonamento_leds.ino
+++ Liste de exercícios/3-escalonamento_leds/escalonamento_leds.ino
@@ -1,18 +1,3 @@
-/* ************************************************************************
-
-Colégio SATC
-Curso Técnico em Mecatrônica
-Disciplina: Sistemas Embarcados
-Turma: 3191
-Professor: Marcos Antonio Jeremias Coelho
-
-Programa: Escalonamento(barra de leds)
-
-Autor: Tiago Fortunato Rosset
-Data: 10/0/2026
-Versão: 1.0
-
-************************************************************************ */
 
 const int sensor = A0;
 const int led1 = 8;
