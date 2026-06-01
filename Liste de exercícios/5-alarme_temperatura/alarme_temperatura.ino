286578c7b6ab859c0c065a330753b3f4e51e70bb
Arthur Schultz Thiel <130780416+thuiel@users.noreply.github.com>
2026-06-01 13:43:13 -0300

    Update alarme_temperatura.ino

--- Liste de exercícios/5-alarme_temperatura/alarme_temperatura.ino
+++ Liste de exercícios/5-alarme_temperatura/alarme_temperatura.ino
@@ -1,18 +1,3 @@
-/* ************************************************************************
-
-Colégio SATC
-Curso Técnico em Mecatrônica
-Disciplina: Sistemas Embarcados
-Turma: 3191
-Professor: Marcos Antonio Jeremias Coelho
-
-Programa: Alarme de Temperatura Simples
-
-Autor: Tiago Fortunato Rosset
-Data: 11/04/2026
-Versão: 1.0
-
-************************************************************************ */
 
 const int sensor = A0;
 const int led = 8;
